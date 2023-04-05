cbc1bb871d881cd1ec7e010ca5fb6f936ede917e
ACER <ACER@LAPTOP-TK89GSO6>
2023-04-05 17:13:36 +0700

    Add project files.

---
+++ Taxi/Taxi.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    internal class Taxi
+    {
+        //properties
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        //method
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("DriverName : {0}, DriverName");
+            if (OnDuty == true)
+            {
+                Console.WriteLine("On Duty : Yes");
+            }
+            if (OnDuty == false)
+            {
+                Console.WriteLine("On Duty : No");
+            }
+            Console.WriteLine("Number of Passenger : {0}", NumPassenger);
+        }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} sudah menjemput penumpang ", DriverName);
+
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} sudah selesai mengantar penumpang", DriverName);
+        }
+    }
+}
